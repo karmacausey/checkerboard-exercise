e0b574c6aad7ed5c986faa0a1697b9faea113311
karmacausey <95538159+karmacausey@users.noreply.github.com>
2022-04-14 18:23:14 -0400

    updated first challenge

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // Your JS goes here
-for (let i = 0; i < 500; i++){
-    var tempDiv = document.createElement('div'); 
-    tempDiv.style.width = "11.1%";
-    tempDiv.style.float = "left";
-    tempDiv.style.paddingBottom = "11.1%";
-    if(i % 2 === 0){
-        tempDiv.style.backgroundColor = "red";
-    } else {
-        tempDiv.style.backgroundColor = "black";
+
+function checkers(){
+         
+    for (let i = 0; i < 500; i++){   
+        var tempDiv = document.createElement('div'); 
+        tempDiv.style.width = "11.1%";
+        tempDiv.style.float = "left";
+        tempDiv.style.paddingBottom = "11.1%"; 
+        if(i % 2 === 0){
+            tempDiv.style.backgroundColor = "red";
+        } else {
+            tempDiv.style.backgroundColor = "black";
+        }
+        document.body.appendChild(tempDiv);
     }
-    document.body.appendChild(tempDiv);
 }
+
+checkers();
