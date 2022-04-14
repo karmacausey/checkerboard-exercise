6f4ea8b30a626691f2fe2b93e088a9198e62135c
karmacausey <95538159+karmacausey@users.noreply.github.com>
2022-04-14 17:26:57 -0400

    first challenge down

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Your JS goes here
+for (let i = 0; i < 500; i++){
+    var tempDiv = document.createElement('div'); 
+    tempDiv.style.width = "11.1%";
+    tempDiv.style.float = "left";
+    tempDiv.style.paddingBottom = "11.1%";
+    if(i % 2 === 0){
+        tempDiv.style.backgroundColor = "red";
+    } else {
+        tempDiv.style.backgroundColor = "black";
+    }
+    document.body.appendChild(tempDiv);
+}
