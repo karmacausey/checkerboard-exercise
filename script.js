1a89af94ef818885371a70c832fb8de7809b8314
karmacausey <95538159+karmacausey@users.noreply.github.com>
2022-04-14 19:24:38 -0400

    random colors solved

--- script.js
+++ script.js
@@ -16,4 +16,22 @@ function checkers(){
     }
 }
 
-checkers();
+function randomColors(){
+    for (let i = 0; i < 500; i++){   
+        var tempDiv = document.createElement('div'); 
+        tempDiv.style.width = "11.1%";
+        tempDiv.style.float = "left";
+        tempDiv.style.paddingBottom = "11.1%";
+        var chars = "0123456789ABCDEF"; 
+        var setColor = "#"
+        for(let i = 0; i < 6; i++){
+             setColor += chars[Math.floor(Math.random()*16)];
+        }
+        tempDiv.style.backgroundColor = setColor;
+        document.body.appendChild(tempDiv);
+    }
+
+}
+
+//checkers();
+randomColors();
