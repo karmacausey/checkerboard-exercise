6046aad886df4cc27a7187d33d05b3f322c34a7d
karmacausey <95538159+karmacausey@users.noreply.github.com>
2022-04-14 21:21:35 -0400

    gottem flashing

--- script.js
+++ script.js
@@ -33,5 +33,61 @@ function randomColors(){
 
 }
 
+function gradient(col1, col2, col3, percent){    
+    for (let i = 0; i < 500; i++){   
+        var tempDiv = document.createElement('div'); 
+        tempDiv.style.width = "11.1%";
+        tempDiv.style.float = "left";
+        tempDiv.style.paddingBottom = "11.1%"; 
+        tempDiv.style.opacity = percent;
+        if(i % 2 === 0){
+            tempDiv.style.backgroundColor = col1;            
+        } else {
+            tempDiv.style.backgroundColor = col3;
+        }
+        document.body.appendChild(tempDiv);
+        
+        
+    }
+    document.body.style.backgroundImage = `linear-gradient(90deg, ${col1} 0%, ${col2} 35%, ${col3} 100%)`;    
+}
+
+function randomColor(){
+    var chars = "0123456789ABCDEF"; 
+    var setColor = "#"
+    for(let i = 0; i < 6; i++){
+         setColor += chars[Math.floor(Math.random()*16)];
+    }
+    return setColor;
+}
+
+function flash(){    
+
+    for (let i = 0; i < 500; i++){ 
+        var tempDiv = document.createElement('div'); 
+        tempDiv.style.width = "11.1%";
+        tempDiv.style.float = "left";
+        tempDiv.style.paddingBottom = "11.1%";        
+        tempDiv.style.backgroundColor = randomColor();              
+        document.body.replaceChild(tempDiv, document.body.childNodes[i]);        
+    }    
+}
+
+function flashing(){
+    
+    for (let i = 0; i < 500; i++){   
+        var tempDiv = document.createElement('div'); 
+        tempDiv.style.width = "11.1%";
+        tempDiv.style.float = "left";
+        tempDiv.style.paddingBottom = "11.1%";        
+        tempDiv.style.backgroundColor = randomColor();
+        document.body.appendChild(tempDiv);
+    }    
+    var interval = setInterval(flash,2000);
+}
+
 //checkers();
-randomColors();
+//randomColors();
+//gradient("#ffff00", "#808080", "#000000", "40%");
+flashing();
+
